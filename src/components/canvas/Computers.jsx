78062c6334b3ffd401547ee3cff2386242ee390b
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-09-11 23:14:35 +0700

    update computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -56,10 +56,10 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
-      shadows={!isMobile}
-      dpr={isMobile ? 1 : [1, 2]} 
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      // frameloop='demand'
+      // shadows={!isMobile}
+      // dpr={isMobile ? 1 : [1, 2]} 
+      // camera={{ position: [20, 3, 5], fov: 25 }}
       // gl={{ preserveDrawingBuffer: false }}
     >
       {/* <Suspense fallback={<CanvasLoader />}>
