c576912c807e1cf8d92f5a8b2a43f0f37991774f
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-09-11 23:16:56 +0700

    update computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -33,46 +33,47 @@ const Computers = ({ isMobile }) => {
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
-  // useEffect(() => {
-  //   // Add a listener for changes to the screen size
-  //   const mediaQuery = window.matchMedia("(max-width: 500px)");
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-  //   // Set the initial value of the `isMobile` state variable
-  //   setIsMobile(mediaQuery.matches);
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
 
-  //   // Define a callback function to handle changes to the media query
-  //   const handleMediaQueryChange = (event) => {
-  //     setIsMobile(event.matches);
-  //   };
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
-  //   // Add the callback function as a listener for changes to the media query
-  //   mediaQuery.addEventListener("change", handleMediaQueryChange);
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-  //   // Remove the listener when the component is unmounted
-  //   return () => {
-  //     mediaQuery.removeEventListener("change", handleMediaQueryChange);
-  //   };
-  // }, []);
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   return (
-    <Canvas
-      // frameloop='demand'
-      // shadows={!isMobile}
-      // dpr={isMobile ? 1 : [1, 2]} 
-      // camera={{ position: [20, 3, 5], fov: 25 }}
-      // gl={{ preserveDrawingBuffer: false }}
-    >
-      {/* <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers isMobile={isMobile} />
-      </Suspense>
+    // <Canvas
+    //   frameloop='demand'
+    //   shadows={!isMobile}
+    //   dpr={isMobile ? 1 : [1, 2]} 
+    //   camera={{ position: [20, 3, 5], fov: 25 }}
+    //   gl={{ preserveDrawingBuffer: true }}
+    // >
+    //   <Suspense fallback={<CanvasLoader />}>
+    //     <OrbitControls
+    //       enableZoom={false}
+    //       maxPolarAngle={Math.PI / 2}
+    //       minPolarAngle={Math.PI / 2}
+    //     />
+    //     <Computers isMobile={isMobile} />
+    //   </Suspense>
 
-      <Preload all /> */}
-    </Canvas>
+    //   <Preload all />
+    // </Canvas>
+    <></>
   );
 };
 
